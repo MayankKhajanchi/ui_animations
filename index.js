f6b5f913207c2fc42d5e5282e472b8d05fb8bc6a
MayankKhajanchi <mayankkhajnchi@gmal.com>
2019-09-12 12:57:04 +0530

    delete card from card columns

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 var card1 = $('.card1');
+// var card2 = $('.card2');
+// var card = $('.card');
 
-TweenLite.from(card1, 1, { opacity: 1, y: 50 });
+$('.card').click(function (event) {
+  TweenMax.to(event.currentTarget, 1, { opacity: 0, y: -50, delay: .5, height: 0, display: 'none' });
+})
+
+// TweenMax.to(card1, 1, { opacity: 0, y: -50, delay: .5, height: 0 });
+// TweenMax.from(card2, 1, { opacity: 0, x: 50 });
+// TweenMax.from(card, 3, { opacity: 0, x: 20 });
